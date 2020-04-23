3708e685e206f07bc5da5c9af1e8e33840d232f1
evantilton <evantilton@gmail.com>
2020-04-23 11:11:49 -0500

    got the background to rotate colors

--- p5-basics/index.js
+++ p5-basics/index.js
@@ -1,15 +1,29 @@
 console.log("In client.js")
 
+
+let x = 0;
+setInterval(function () {
+    if (x >= 255) {
+        x = 0;
+    } else {
+        x = x+1;
+    }
+}, 100);
 //creates the canvas we are drawing on
 function setup() {
     createCanvas(400, 400);
 }
-
+x = 0;
 //sets the color
 function draw() {
-    background(220,0,200);
+    background(x);
     fill(255, 204, 0);
     rectMode(CENTER);
     rect(200,150,150,150)
     line(0,50,300,400)
 }
+
+function colorUp() {
+    console.log("hello");
+}
+

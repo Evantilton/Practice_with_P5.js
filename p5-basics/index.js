3d0a11e49001eeecdbce4abe6f476f6ff230cf38
evantilton <evantilton@gmail.com>
2020-04-23 11:20:44 -0500

    made the rectangle change color randomly too

--- p5-basics/index.js
+++ p5-basics/index.js
@@ -6,6 +6,7 @@ setInterval(function () {
     x = Math.floor(Math.random() * 256);
     y = Math.floor(Math.random() * 256);
     z = Math.floor(Math.random() * 256);
+    random = Math.floor(Math.random() * 10);
 }, 1000);
 //creates the canvas we are drawing on
 function setup() {
@@ -16,8 +17,8 @@ y = 0;
 z = 0;
 //sets the color
 function draw() {
-    background(x,y,z);
-    fill(255, 204, 0);
+    background(x*random,y*random,z*random);
+    fill(x+10, y+10, z+10);
     rectMode(CENTER);
     rect(200,150,150,150)
     line(0,50,300,400)

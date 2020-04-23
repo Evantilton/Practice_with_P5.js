dfe31da7f3774926c89b8b436b67410b5286c35c
evantilton <evantilton@gmail.com>
2020-04-23 11:15:38 -0500

    random colors

--- p5-basics/index.js
+++ p5-basics/index.js
@@ -3,20 +3,20 @@ console.log("In client.js")
 
 let x = 0;
 setInterval(function () {
-    if (x >= 255) {
-        x = 0;
-    } else {
-        x = x+1;
-    }
-}, 100);
+    x = Math.floor(Math.random() * 256);
+    y = Math.floor(Math.random() * 256);
+    z = Math.floor(Math.random() * 256);
+}, 1000);
 //creates the canvas we are drawing on
 function setup() {
     createCanvas(400, 400);
 }
 x = 0;
+y = 0;
+z = 0;
 //sets the color
 function draw() {
-    background(x);
+    background(x,y,z);
     fill(255, 204, 0);
     rectMode(CENTER);
     rect(200,150,150,150)

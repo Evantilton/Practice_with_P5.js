c3828f66f77c7d4ae13fde677217f30255464378
evantilton <evantilton@gmail.com>
2020-04-23 09:35:50 -0500

    added color

--- p5-basics/index.js
+++ p5-basics/index.js
@@ -1,7 +1,13 @@
+console.log("In client.js")
+
+//creates the canvas we are drawing on
 function setup() {
     createCanvas(400, 400);
 }
 
+//sets the color
 function draw() {
-    background(220);
+    background(100,100);
+    fill(255, 204, 0);
+    rect(100,50,10,10)
 }
